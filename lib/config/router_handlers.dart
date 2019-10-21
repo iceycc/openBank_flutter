25936604206a0cd44cd73d38d1e2c36a3cda2918
iceyang <w@icey.cc>
2019-10-21 17:01:00 +0800

    add

--- lib/config/router_handlers.dart
+++
@@ -1,8 +0,0 @@
-import 'package:fluro/fluro.dart';
-import 'package:flutter/material.dart';
-import 'package:openbank/page/login/login.dart';
-
-var loginHandle = Handler(
-    handlerFunc: (BuildContext context, Map<String, List<String>> parameters) {
-  return LoginView();
-});
